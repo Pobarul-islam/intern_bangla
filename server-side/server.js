f2d718e92f4a89d796fc728a9276395bef788af2
Pobarul-islam <pobarul3@gmail.com>
2023-05-09 16:50:31 +0600

    server side setup

---
+++ server-side/server.js
@@ -0,0 +1,29 @@
+// import 
+import colors from "colors";
+import express from "express";
+import dotenv from 'dotenv';
+
+// Dot env config 
+dotenv.config()
+
+
+// rest object 
+const app = express();
+
+
+// route 
+app.get('/', (req, res) => {
+    res.send("welcome to intern_bangla website")
+})
+
+
+
+// port 
+const PORT = process.env.PORT || 5000;
+
+
+
+// listen 
+app.listen(PORT, () => {
+    console.log(`intern_bangla server is running !!`.bgMagenta)
+})
