54cb465c9b032bcc95c8bd512b2d0d0281a65c0f
Pobarul-islam <pobarul3@gmail.com>
2023-05-13 13:16:06 +0600

    filtering worked

--- server-side/controllers/jobController.js
+++ server-side/controllers/jobController.js
@@ -1,5 +1,6 @@
 import jobsModel from "../models/jobsModel.js";
 import mongoose from "mongoose";
+import moment from "moment";
 // Create Job Controller
 
 export const createJobController = async (req, res, next) => {
@@ -24,66 +25,106 @@ export const getAllJobsController = async (req, res, next) => {
 
 // Update jobs
 export const updateJobController = async (req, res, next) => {
-    const { id } = req.params
-    const { company, position } = req.body
-    // validation 
-    if (!company || !position) {
-        next('Please provide all fields')
-    }
+  const { id } = req.params;
+  const { company, position } = req.body;
+  // validation
+  if (!company || !position) {
+    next("Please provide all fields");
+  }
 
-    // find job 
-    const job = await jobsModel.findOne({ _id: id })
-    
-    // validation 
-    if (!job) {
-        next(`no jobs found with this id ${id}`)
-    }
+  // find job
+  const job = await jobsModel.findOne({ _id: id });
 
-    if (!req.user.userId === job.createdBy.toString()) {
-        return;
-        next('You are not authorized to update this job')
-    }
-    const updateJob = await jobsModel.findOneAndUpdate({ _id: id }, req.body, {
-        new: true,
-        runValidators: true,
-    })
+  // validation
+  if (!job) {
+    next(`no jobs found with this id ${id}`);
+  }
 
-    // res
-    res.status(200).json({ updateJob })
+  if (!req.user.userId === job.createdBy.toString()) {
+    return;
+    next("You are not authorized to update this job");
+  }
+  const updateJob = await jobsModel.findOneAndUpdate({ _id: id }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  // res
+  res.status(200).json({ updateJob });
 };
-
-
 
 // Delete jobs controller
 
 export const deleteJobController = async (req, res, next) => {
-    const { id } = req.params
-    // find job 
-    const job = await jobsModel.findOne({ _id: id })
-    // validation 
-    if (!job) {
-        next(`No Job found with this id ${id}`)
-    }
-    if (!req.user.userId === job.createdBy.toString()) {
-        next('Your not authorize to delete this job')
-    }
+  const { id } = req.params;
+  // find job
+  const job = await jobsModel.findOne({ _id: id });
+  // validation
+  if (!job) {
+    next(`No Job found with this id ${id}`);
+  }
+  if (!req.user.userId === job.createdBy.toString()) {
+    next("Your not authorize to delete this job");
+  }
 
-    await job.deleteOne();
-    res.status(200).json({message: "Success, Job Deleted"})
-}
-
-
+  await job.deleteOne();
+  res.status(200).json({ message: "Success, Job Deleted" });
+};
 
 // Jobs stats and filter
 
 export const jobStatsController = async (req, res) => {
-    const stats = await jobsModel.aggregate([
-      // search by user jobs
-      {
-        $match: {
-          createdBy: new mongoose.Types.ObjectId(req.user.userId),
+  const stats = await jobsModel.aggregate([
+    // search by user jobs
+    {
+      $match: {
+        createdBy: new mongoose.Types.ObjectId(req.user.userId),
+      },
+    },
+    {
+      $group: {
+        _id: "$status",
+        count: { $sum: 1 },
+      },
+    },
+  ]);
+
+  // default stats
+
+  const defaultStats = {
+    pending: stats.pending || 0,
+    reject: stats.rejict || 0,
+    interview: stats.interview || 0,
+  };
+
+  // monthly yearly stats
+
+  let monthlyApplication = await jobsModel.aggregate([
+    {
+      $match: {
+        createdBy: new mongoose.Types.ObjectId(req.user.userId),
+      },
+    },
+    {
+      $group: {
+        _id: {
+          year: { $year: "$createdAt" },
+          month: { $month: "$createdAt" },
+        },
+        count: {
+          $sum: 1,
         },
       },
-    ]);
-    res.status(200).json({totalJob:stats.length,stats });
+    },
+  ]);
+
+  monthlyApplication = monthlyApplication.map(item => {
+    const { _id: { year, month }, count } = item
+    const date = moment().month(-1).year(year).format('MMM Y')
+    return { date, count }
+  })
+    .reverse();
+  res
+    .status(200)
+    .json({ totalJob: stats.length, defaultStats, monthlyApplication });
 };
