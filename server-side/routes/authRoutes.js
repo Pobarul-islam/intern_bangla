d7749bb6bbe5334796b6b6168aaa495f118d1335
Pobarul-islam <pobarul3@gmail.com>
2023-05-13 20:45:10 +0600

    security fixed

--- server-side/routes/authRoutes.js
+++ server-side/routes/authRoutes.js
@@ -1,11 +1,22 @@
 import express from "express";
 import { loginController, registerController } from "../controllers/authController.js";
+import rateLimit from "express-rate-limit";
+
+
+// ip limiter 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 
 // routes object
 const router = express.Router();
 
 // REGISTER ROUTE
-router.post("/register", registerController);
+router.post("/register", limiter, registerController);
 
 // LOGIN ROUTE 
 router.post("/login", loginController)
--- server-side/server.js
+++ server-side/server.js
@@ -1,54 +1,56 @@
-// packages imports  
+// packages imports
 import colors from "colors";
 import express from "express";
-import dotenv from 'dotenv';
-import connectDB from "./config/db.js";
+import dotenv from "dotenv";
 import morgan from "morgan";
 import cors from "cors";
 import "express-async-errors";
-// routes import 
-import testRoutes from './routes/testRoutes.js'
-import authRoutes from './routes/authRoutes.js'
+
+// security imports
+import helmet from "helmet";
+import xss from 'xss-clean';
+import mongoSanitize from 'express-mongo-sanitize';
+// files import
+import connectDB from "./config/db.js";
+
+// routes import
+import testRoutes from "./routes/testRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 import errorMiddelware from "./middelwares/errorMiddelware.js";
-import userRoutes from './routes/userRoutes.js';
-import jobsRoutes from './routes/jobsRoutes.js';
+import userRoutes from "./routes/userRoutes.js";
+import jobsRoutes from "./routes/jobsRoutes.js";
 
+// Dot env config
+dotenv.config();
 
-// Dot env config 
-dotenv.config()
-
-
-// mongodb connection 
+// mongodb connection
 connectDB();
 
-// rest object 
+// rest object
 const app = express();
 
 // middelwares
+app.use(helmet());
+app.use(xss());
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+app.use(mongoSanitize());
 
+// route
 
-
-
-// route 
-
-app.use('/api/v1/test', testRoutes)
+app.use("/api/v1/test", testRoutes);
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/user", userRoutes);
-app.use("/api/v1/job", jobsRoutes)
+app.use("/api/v1/job", jobsRoutes);
 
-// validation middelware 
+// validation middelware
 app.use(errorMiddelware);
 
-
-// port 
+// port
 const PORT = process.env.PORT || 5000;
 
-
-
-// listen 
+// listen
 app.listen(PORT, () => {
-    console.log(`intern_bangla server is running !!`.bgBlue)
-})
+  console.log(`intern_bangla server is running !!`.bgBlue);
+});
