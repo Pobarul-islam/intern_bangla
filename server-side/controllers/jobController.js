94e22579f1b713d89fc3bcea42a34ab1188d0f51
Pobarul-islam <pobarul3@gmail.com>
2023-05-13 19:16:07 +0600

    searching and sorting filters worked

--- server-side/controllers/jobController.js
+++ server-side/controllers/jobController.js
@@ -16,7 +16,49 @@ export const createJobController = async (req, res, next) => {
 // Get jobs
 
 export const getAllJobsController = async (req, res, next) => {
-  const jobs = await jobsModel.find({ createdBy: req.user.userId });
+
+  const { status, workType, search, sort} = req.query
+  // conditions for searching filters 
+  const queryObject = {
+    createdBy: req.user.userId
+  }
+
+  // logic filters
+
+  if (status && status !== 'all') {
+      queryObject.status = status
+  }
+
+  if (workType && workType !== 'all') {
+    queryObject.workType = workType;
+  }
+
+  if (search) {
+    queryObject.position = { $regex: search, $options: 'i' };
+  }
+
+  let queryResult = jobsModel.find(queryObject)
+
+  // sorting
+  if (sort === 'latest') {
+    queryResult = queryResult.sort('-createdAt')
+  }
+
+  if (sort === 'oldest') {
+    queryResult = queryResult.sort('createdAt')
+  }
+  if (sort === 'a-z') {
+    queryResult = queryResult.sort('position')
+  }
+  if (sort === 'z-a') {
+    queryResult = queryResult.sort('-position')
+  }
+  
+
+  const jobs = await queryResult;
+  
+
+  // const jobs = await jobsModel.find({ createdBy: req.user.userId });
   res.status(200).json({
     totalJobs: jobs.length,
     jobs,
--- server-side/models/jobsModel.js
+++ server-side/models/jobsModel.js
@@ -1,33 +1,36 @@
 import mongoose from "mongoose";
-const jobSchema = new mongoose.Schema({
+const jobSchema = new mongoose.Schema(
+  {
     company: {
-        type: String,
-        required: [true, "Company name is require"],
+      type: String,
+      required: [true, "Company name is require"],
     },
     position: {
-        type: String,
-        required: [true, 'Job position is required'],
-        maxlength: 100
-
+      type: String,
+      required: [true, "Job position is required"],
+      maxlength: 100,
     },
     status: {
-        type: ['Pending', 'Reject', 'Interview'],
-        default: 'Pending'
+      type: String,
+      enum: ["Pending", "Reject", "Interview"],
+      default: "Pending",
     },
     workType: {
-        type: String,
-        enum: ['full-time', 'part-time', 'internship', 'contaract'],
-        default: 'full-time'
+      type: String,
+      enum: ["full-time", "part-time", "internship", "contaract"],
+      default: "full-time",
     },
     workLocation: {
-        type: String,
-        default: 'Bangladesh',
-        required:[true, 'Work location is required']
+      type: String,
+      default: "Bangladesh",
+      required: [true, "Work location is required"],
     },
     createdBy: {
-        type: mongoose.Types.ObjectId,
-        ref: 'User'
-    }
-}, {timestamps: true})
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
-export default mongoose.model('Job', jobSchema);
+export default mongoose.model("Job", jobSchema);
