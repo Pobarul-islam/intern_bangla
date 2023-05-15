a3e1c9e1b0d3792eacf816e097841932d0c46b31
Pobarul-islam <pobarul3@gmail.com>
2023-05-15 16:13:29 +0600

    register form created

--- client-side/src/Pages/Login/Register.tsx
+++ client-side/src/Pages/Login/Register.tsx
@@ -1,54 +1,98 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-    return (
-      <div>
-        <div className="hero min-h-screen bg-base-200">
-          <div className="hero-content flex-col lg:flex-row-reverse">
-            <div className="text-center lg:text-left">
-              <h1 className="text-5xl font-bold">Login now!</h1>
-              <p className="py-6">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-              </p>
-            </div>
-            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-              <div className="card-body">
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Email</span>
-                  </label>
-                  <input
-                    type="text"
-                    placeholder="email"
-                    className="input input-bordered"
-                  />
-                </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Password</span>
-                  </label>
-                  <input
-                    type="text"
-                    placeholder="password"
-                    className="input input-bordered"
-                  />
-                  <label className="label">
-                    <a href="#" className="label-text-alt link link-hover">
-                      Forgot password?
-                    </a>
-                  </label>
-                </div>
-                <div className="form-control mt-6">
-                  <button className="btn btn-primary">Login</button>
-                </div>
-              </div>
-            </div>
+  // const [name, setName] = useState("");
+  // const [lastName, setLastName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+
+
+  const [values, setValues] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    password: ""
+  })
+  
+  // Submit form function 
+  const handleOnSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault()
+    try {
+      console.log(values)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleOnChange = (e: { target: {
+    [x: string]: any; value: any; 
+}; }) => {
+    const value = e.target.value
+    setValues({
+      ...values,
+      [e.target.name]: value,
+    })
+  }
+
+
+  return (
+    <div className="container mx-auto p-4 bg-white">
+      <div className="w-full md:w-1/2 lg:w-1/3 mx-auto">
+        <h1 className="text-lg font-bold">Register</h1>
+        <form onSubmit={handleOnSubmit} className="flex flex-col mt-4">
+          <input
+            onChange={handleOnChange}
+            type="text"
+            name="name"
+            className="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+            placeholder="Name"
+            value={values.name}
+          />
+          <input
+            onChange={handleOnChange}
+            type="text"
+            name="lastName"
+            className="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+            placeholder="Last name"
+            value={values.lastName}
+          />
+
+          <input
+            onChange={handleOnChange}
+            type="email"
+            name="email"
+            className="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+            placeholder="Email address"
+            value={values.email}
+          />
+          <input
+            onChange={handleOnChange}
+            type="password"
+            name="password"
+            className="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+            placeholder="Password"
+            value={values.password}
+          />
+
+          <button
+            type="submit"
+            className="mt-4 px-4 py-3  leading-6 text-base rounded-md border border-transparent text-white focus:outline-none bg-blue-500 hover:text-white focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 cursor-pointer inline-flex w-full justify-center items-center font-medium"
+          >
+            Register
+          </button>
+          <div className="flex flex-col items-center mt-5">
+            <p className="mt-1 text-xs font-light text-gray-500">
+              Register already?
+              <Link to="/login" className="ml-1 font-medium text-blue-400">
+                Login now
+              </Link>
+            </p>
           </div>
-        </div>
+        </form>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Register;
